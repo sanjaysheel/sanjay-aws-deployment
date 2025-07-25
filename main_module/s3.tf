8e04c3d64778c9504826b47f04c9f98466b30de1
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2025-07-26 03:08:39 +0530

    bucket-name changed

--- main_module/s3.tf
+++ main_module/s3.tf
@@ -1,8 +1,8 @@
 module "s3_bucket" {
-  source      = "../modules/aws/s3"       # Path to the S3 module
-  bucket_name = "${var.environment}-data" # Environment-specific bucket name suffix
-  environment = var.environment           # Use an environment variable (e.g., dev, prod)
-  tags        = local.common_tags         # Pass common tags to the module
+  source      = "../modules/aws/s3"              # Path to the S3 module
+  bucket_name = "${var.environment}-data-test-1" # Environment-specific bucket name suffix
+  environment = var.environment                  # Use an environment variable (e.g., dev, prod)
+  tags        = local.common_tags                # Pass common tags to the module
 }
 
 output "bucket_name" {
