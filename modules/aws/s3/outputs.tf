6a18f7aa3dc660f1fbabec9ebbe84f73289e3afb
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2025-07-26 03:33:28 +0530

    resolved the fodler wise all issue

--- modules/aws/s3/outputs.tf
+++ modules/aws/s3/outputs.tf
@@ -1,15 +1,11 @@
-# output "bucket_name" {
-#   value = aws_s3_bucket.this.bucket
-# }
-
-# output "bucket_arn" {
-#   value = aws_s3_bucket.this.arn
-# }
-
-# output "bucket_domain_name" {
-#   value = aws_s3_bucket.this.bucket_domain_name
-# }
 output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.bucket.bucket
+  value = aws_s3_bucket.this.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.this.bucket_domain_name
 }
