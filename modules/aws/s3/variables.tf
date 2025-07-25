916ac949983b217514c4fbb7c6a57803596e01b2
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2025-07-26 03:30:57 +0530

    fixed the sisue

--- modules/aws/s3/variables.tf
+++ modules/aws/s3/variables.tf
@@ -1,37 +1,21 @@
-# variable "bucket_name" {
-#   description = "The name of the S3 bucket"
-#   type        = string
-# }
-
-# variable "environment" {
-#   description = "The environment (dev, stag, prod)"
-#   type        = string
-# }
-
-# variable "tags" {
-#   description = "A map of tags to assign to the resource"
-#   type        = map(string)
-#   default     = {}
-# }
-
-# variable "force_destroy" {
-#   description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
-#   type        = bool
-#   default     = false
-# }
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
 
-variable "acl" {
-  description = "The canned ACL to apply"
+variable "environment" {
+  description = "The environment (dev, stag, prod)"
   type        = string
-  default     = "private"
 }
 
 variable "tags" {
-  description = "A map of tags to apply to the bucket"
+  description = "A map of tags to assign to the resource"
   type        = map(string)
   default     = {}
+}
+
+variable "force_destroy" {
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
+  type        = bool
+  default     = false
 }
