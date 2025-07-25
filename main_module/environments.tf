326a789f46b28e27863794747e829f2a72af7a00
Sanjaysheel <33994199+sanjaysheel@users.noreply.github.com>
2025-07-26 02:59:02 +0530

    fixed the issue to create s3 resource

--- main_module/environments.tf
+++ main_module/environments.tf
@@ -6,18 +6,7 @@ locals {
 resource "aws_iam_role" "environment_role" {
   name = "environment-${var.environment}-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
+  assume_role_policy = file("../modules/template/iam_assume_role_policy.json")
 
   tags = {
     Environment = var.environment
